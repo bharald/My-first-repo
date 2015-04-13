195eef8f269fc37e4ab0816bad9d5fb1e8ca58ee
Harald Brorsson <harald.brorsson@gmail.com>
2015-04-13 18:53:31 +0200

    Add PlayerCharacter class

---
+++ core/src/Stages/GameStage.java
@@ -0,0 +1,7 @@
+package Stages;
+
+/**
+ * Created by Harald on 2015-04-13.
+ */
+public class GameStage {
+}
---
+++ core/src/Stages/PlayerCharacter.java
@@ -0,0 +1,20 @@
+package Stages;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
+/**
+ * Created by Harald on 2015-04-13.
+ */
+public class PlayerCharacter extends Image {
+    private Texture texture;
+    public PlayerCharacter(){
+        texture=new Texture(Gdx.files.internal("badlogic.jpg"));
+    }
+    @Override
+    public void draw(Batch batch, float parentAlpha){
+        batch.draw(texture,0,0);
+    }
+}
